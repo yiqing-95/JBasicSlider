cc8a878b0ad6875ea742cc516cfb53b55be790a6
yiqing-95 <61391362@qq.com>
2012-04-10 17:48:34 +0800

    init the project

---
+++ bjqs/JBasicSlider.php
@@ -0,0 +1,174 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: yiqing
+ * Date: 12-4-10
+ * Time: 下午4:55
+ *------------------------------------------------------------
+ *                 _            _
+ *                (_)          (_)
+ *        _   __  __   .--. _  __   _ .--.   .--./)
+ *       [ \ [  ][  |/ /'`\' ][  | [ `.-. | / /'`\;
+ *        \ '/ /  | || \__/ |  | |  | | | | \ \._//
+ *      [\_:  /  [___]\__.; | [___][___||__].',__`
+ *       \__.'            |__]             ( ( __))
+ *
+ *--------------------------------------------------------------
+ * To change this template use File | Settings | File Templates.
+ */
+class JBasicSlider extends CWidget
+{
+    /**
+     * @static
+     * @param bool $hashByName
+     * @return string
+     * return this widget assetsUrl
+     */
+    public static function getAssetsUrl($hashByName = false)
+    {
+        // return CHtml::asset(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'assets', $hashByName);
+        return Yii::app()->getAssetManager()->publish(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'assets', $hashByName, -1, YII_DEBUG);
+    }
+
+
+    /**
+     * @var string
+     */
+    public $baseUrl;
+
+    /**
+     * @var bool
+     */
+    public $debug = YII_DEBUG;
+
+    /**
+     * @var \CClientScript
+     */
+    protected $cs;
+
+    /**
+     * @var array|string
+     * -------------------------
+     * the options will be passed to the underlying plugin
+     *   eg:  js:{key:val,k2:v2...}
+     *   array('key'=>$val,'k'=>v2);
+     * -------------------------
+     */
+    public $options = array();
+
+
+    /**
+     * @var string
+     */
+    public $selector;
+
+
+    /**
+     * @return JBasicSlider
+     */
+    public function publishAssets()
+    {
+        if (empty($this->baseUrl)) {
+            $assetsPath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'assets';
+            if ($this->debug == true) {
+                $this->baseUrl = Yii::app()->assetManager->publish($assetsPath, false, -1, true);
+            } else {
+                $this->baseUrl = Yii::app()->assetManager->publish($assetsPath);
+            }
+        }
+        return $this;
+    }
+
+
+    /**
+     * @throws CException
+     */
+    public function init()
+    {
+        if (empty($this->selector)) {
+            throw new CException('you must specify a selector for uising this widget!');
+        }
+        parent::init();
+        $this->cs = Yii::app()->getClientScript();
+        // publish assets and register css/js files
+        $this->publishAssets();
+        // register necessary js file and css files
+        $this->cs->registerCoreScript('jquery');
+
+        if($this->debug == true){
+            $this->registerScriptFile('js/basic-jquery-slider.js', CClientScript::POS_HEAD);
+        }else{
+            $this->registerScriptFile('js/basic-jquery-slider.min.js', CClientScript::POS_HEAD);
+        }
+
+        $this->registerCssFile('css/basic-jquery-slider.css');
+
+        $options = empty($this->options) ? '' : CJavaScript::encode($this->options);
+        $jsSetup = <<<JS_INIT
+           $("{$this->selector}").bjqs({$options});
+JS_INIT;
+        $this->cs->registerScript(__CLASS__ . '#' . $this->getId(), $jsSetup, CClientScript::POS_READY);
+    }
+
+    /**
+     * @param $name
+     * @param $value
+     */
+    public function __set($name, $value)
+    {
+        try {
+            //shouldn't swallow the parent ' __set operation
+            parent::__set($name, $value);
+        } catch (Exception $e) {
+            $this->options[$name] = $value;
+        }
+    }
+
+    /**
+     * @param $fileName
+     * @param int $position
+     * @return JBasicSlider
+     * @throws InvalidArgumentException
+     */
+    protected function registerScriptFile($fileName, $position = CClientScript::POS_END)
+    {
+        if (is_string($fileName)) {
+            $jsFiles = explode(',', $fileName);
+        } elseif (is_array($fileName)) {
+            $jsFiles = $fileName;
+        } else {
+            throw new InvalidArgumentException('you must give a string or array as first argument , but now you give' . var_export($fileName, true));
+        }
+        foreach ($jsFiles as $jsFile) {
+            $jsFile = trim($jsFile);
+            $this->cs->registerScriptFile($this->baseUrl . '/' . ltrim($jsFile, '/'), $position);
+        }
+        return $this;
+    }
+
+    /**
+     * @param $fileName   you can give many css files : 'xx.css','xx2.css',
+     * @return JBasicSlider
+     * @throws InvalidArgumentException
+     */
+    protected function registerCssFile($fileName)
+    {
+        $cssFiles = func_get_args();
+        foreach ($cssFiles as $cssFile) {
+            if (is_string($cssFile)) {
+                $cssFiles2 = explode(',', $cssFile);
+            } elseif (is_array($cssFile)) {
+                $cssFiles2 = $cssFile;
+            } else {
+                throw new InvalidArgumentException('you must give a string or array as first argument , but now you give' . var_export($cssFiles, true));
+            }
+            foreach ($cssFiles2 as $css) {
+                $this->cs->registerCssFile($this->baseUrl . '/' . ltrim($css, '/'));
+            }
+        }
+        // $this->cs->registerCssFile($this->assetsUrl . '/vendors/' .$fileName);
+        return $this;
+    }
+
+
+}
